df80568ba480b2356cb2bbb5d40d6b586ee35ce6
Masato Naka <masatonaka1989@gmail.com>
2024-04-28 22:50:36 +0900

    build with ko

--- pkg/visualize/visualize.go
+++ pkg/visualize/visualize.go
@@ -1,13 +1,29 @@
 package visualize
 
 import (
+	"context"
+	"encoding/base64"
+	"fmt"
 	"log"
 	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/chromedp/chromedp"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
 	"github.com/go-echarts/snapshot-chromedp/render"
 	"github.com/nakamasato/cloud-run-slack-bot/pkg/monitoring"
+)
+
+// https://github.com/go-echarts/snapshot-chromedp/blob/47575f6f0d3957501fff8cd8fa89c6f3e97916a4/render/chromedp.go#L15-L21
+const (
+	HTML               = "html"
+	FileProtocol       = "file://"
+	EchartsInstanceDom = "div[_echarts_instance_]"
+	CanvasJs           = "echarts.getInstanceByDom(document.querySelector('div[_echarts_instance_]'))" +
+		".getDataURL({type: '%s', pixelRatio: %d, excludeComponents: ['toolbox']})"
 )
 
 // generate random data
@@ -75,7 +91,7 @@ func VisualizeSample(fileName string) error {
 		AddSeries("B", *getLineData(generateRandomData())).
 		AddSeries("C", *getLineData(generateRandomData())).
 		AddSeries("D", *getLineData(generateRandomData()))
-	return render.MakeChartSnapshot(line.RenderContent(), fileName)
+	return MakeChartSnapshot(line.RenderContent(), fileName)
 }
 
 func getLineData(data *monitoring.TimeSeries) *[]opts.LineData {
@@ -84,4 +100,99 @@ func getLineData(data *monitoring.TimeSeries) *[]opts.LineData {
 		items = append(items, opts.LineData{Value: v, XAxisIndex: i})
 	}
 	return &items
+}
+
+// https://github.com/go-echarts/snapshot-chromedp/blob/47575f6f0d3957501fff8cd8fa89c6f3e97916a4/render/chromedp.go#L42-L56
+func MakeChartSnapshot(content []byte, image string) error {
+	path, file := filepath.Split(image)
+	suffix := filepath.Ext(file)[1:]
+	fileName := file[0 : len(file)-len(suffix)-1]
+
+	config := &render.SnapshotConfig{
+		RenderContent: content,
+		Path:          path,
+		FileName:      fileName,
+		Suffix:        suffix,
+		Quality:       1,
+		KeepHtml:      false,
+	}
+	return MakeSnapshot(config)
+}
+
+func MakeSnapshot(config *render.SnapshotConfig) error {
+	path := config.Path
+	fileName := config.FileName
+	content := config.RenderContent
+	quality := config.Quality
+	suffix := config.Suffix
+	keepHtml := config.KeepHtml
+	htmlPath := config.HtmlPath
+
+	if htmlPath == "" {
+		htmlPath = path
+	}
+
+	if !filepath.IsAbs(path) {
+		path, _ = filepath.Abs(path)
+	}
+
+	if !filepath.IsAbs(htmlPath) {
+		htmlPath, _ = filepath.Abs(htmlPath)
+	}
+
+	var ctx context.Context
+	var cancel context.CancelFunc
+	if chromedpRemoteUrl := os.Getenv("CHROMEDP_REMOTE_URL"); chromedpRemoteUrl == "" { // "wss://localhost:9222"
+		ctx = context.Background()
+	} else {
+		ctx, cancel = chromedp.NewRemoteAllocator(context.Background(), chromedpRemoteUrl)
+		defer cancel()
+	}
+
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	htmlFullPath := filepath.Join(htmlPath, fileName+"."+HTML)
+
+	if !keepHtml {
+		defer func() {
+			err := os.Remove(htmlFullPath)
+			if err != nil {
+				log.Printf("Failed to delete the file(%s), err: %s\n", htmlFullPath, err)
+			}
+		}()
+	}
+
+	err := os.WriteFile(htmlFullPath, content, 0o644)
+	if err != nil {
+		return err
+	}
+
+	if quality < 1 {
+		quality = 1
+	}
+
+	var base64Data string
+	executeJS := fmt.Sprintf(CanvasJs, suffix, quality)
+	err = chromedp.Run(ctx,
+		chromedp.Navigate(fmt.Sprintf("%s%s", FileProtocol, htmlFullPath)),
+		chromedp.WaitVisible(EchartsInstanceDom, chromedp.ByQuery),
+		chromedp.Evaluate(executeJS, &base64Data),
+	)
+	if err != nil {
+		return err
+	}
+
+	imgContent, err := base64.StdEncoding.DecodeString(strings.Split(base64Data, ",")[1])
+	if err != nil {
+		return err
+	}
+
+	imageFullPath := filepath.Join(path, fmt.Sprintf("%s.%s", fileName, suffix))
+	if err := os.WriteFile(imageFullPath, imgContent, 0o644); err != nil {
+		return err
+	}
+
+	log.Printf("Wrote %s.%s success", fileName, suffix)
+	return nil
 }
