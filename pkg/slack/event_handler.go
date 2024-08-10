3056e73fe2ac96bca76056a9d2dc10088d75f42d
Masato Naka <masatonaka1989@gmail.com>
2024-08-10 20:22:15 +0900

    add text block

--- pkg/slack/event_handler.go
+++ pkg/slack/event_handler.go
@@ -281,6 +281,7 @@ func (h *SlackEventHandler) getServiceMetrics(ctx context.Context, channelId, sv
 	blocks := []slack.Block{
 		// slack.NewImageBlock(f.URLPrivate, "Request Count", "Request Count", slack.NewTextBlockObject("title", "title", false, false)),
 		// slack.NewImageBlock(f.URLPrivate, "Resource Count", "resource_count", slack.NewTextBlockObject(slack, text string, emoji bool, verbatim bool))
+		slack.NewTextBlockObject("text", fmt.Sprintf("`%s`: %s", svcName, f.URLPrivate), false, false),
 		slack.ImageBlock{
 			Type:     "image",
 			ImageURL: "https://files.slack.com/files-pri/TK8MCGJHH-F07GD216W4S/cloud-run-slack-bot-metrics.png",
